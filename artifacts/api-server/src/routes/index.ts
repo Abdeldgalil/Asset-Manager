c2deaef494b6aec75ec23c631ba4814da453a9a0
Abdeldgalil <djaliltalbi642@gmail.com>
2026-07-11 10:40:26 +0100

    Add imageRouter to main routes

--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -1,10 +1,13 @@
+
 import { Router, type IRouter } from "express";
 import healthRouter from "./health";
 import storiesRouter from "./stories";
+import imageRouter from "./stories/image";
 
 const router: IRouter = Router();
 
 router.use(healthRouter);
 router.use(storiesRouter);
+router.use(imageRouter);
 
 export default router;
